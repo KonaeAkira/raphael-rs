e146c725b0a2fca2f8dc86d4a4e37de4ad712085
Truong Hoang Long <longtruong2411@gmail.com>
2024-05-04 20:37:47 +0200

    Override debug trait for ScaledU32

--- src/game/units/scaled_integer.rs
+++ src/game/units/scaled_integer.rs
@@ -1,28 +1,32 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+use core::fmt;
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct ScaledU32<const C: u32> {
-    value: u32,
+    inner_value: u32,
 }
 
 impl<const C: u32> ScaledU32<C> {
     pub const fn new(value: u32) -> Self {
-        Self { value: value * C }
+        Self {
+            inner_value: value * C,
+        }
     }
 
     pub const fn saturating_add(self, other: Self) -> Self {
         Self {
-            value: self.value.saturating_add(other.value),
+            inner_value: self.inner_value.saturating_add(other.inner_value),
         }
     }
 
     pub const fn saturating_sub(self, other: Self) -> Self {
         Self {
-            value: self.value.saturating_sub(other.value),
+            inner_value: self.inner_value.saturating_sub(other.inner_value),
         }
     }
 
     pub const fn scale(self, mul: u32, div: u32) -> Self {
         Self {
-            value: self.value * mul / div,
+            inner_value: self.inner_value * mul / div,
         }
     }
 }
@@ -30,13 +34,22 @@ impl<const C: u32> ScaledU32<C> {
 impl<const C: u32> std::convert::From<f32> for ScaledU32<C> {
     fn from(value: f32) -> Self {
         Self {
-            value: (value * C as f32).ceil() as u32,
+            inner_value: (value * C as f32).ceil() as u32,
         }
     }
 }
 
 impl<const C: u32> std::convert::From<ScaledU32<C>> for f32 {
     fn from(value: ScaledU32<C>) -> Self {
-        value.value as f32 / C as f32
+        value.inner_value as f32 / C as f32
+    }
+}
+
+impl<const C: u32> fmt::Debug for ScaledU32<C> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ScaledU32")
+            .field("value", &(self.inner_value as f32 / C as f32))
+            .field("inner_value", &self.inner_value)
+            .finish()
     }
 }
