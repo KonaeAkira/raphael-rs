a1fe84cc949d943dd7378e6d9b888e43286d8ecc
KonaeAkira <longtruong2411@gmail.com>
2024-05-16 21:09:02 +0200

    Solve for shortest finish sequence

--- src/solvers/finish_solver.rs
+++ src/solvers/finish_solver.rs
@@ -7,6 +7,7 @@ use rustc_hash::FxHashMap as HashMap;
 
 use super::actions::{DURABILITY_ACTIONS, PROGRESS_ACTIONS};
 
+const INF_PROGRESS: Progress = Progress::new(100000);
 const SEARCH_ACTIONS: ActionMask = PROGRESS_ACTIONS.union(DURABILITY_ACTIONS);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -49,8 +50,6 @@ struct ReducedState {
 }
 
 impl ReducedState {
-    pub const INF_PROGRESS: Progress = Progress::new(100_000);
-
     pub fn from_state(state: &InProgress) -> ReducedState {
         ReducedState {
             durability: state.durability,
@@ -64,7 +63,7 @@ impl ReducedState {
         InProgress {
             durability: self.durability,
             cp: self.cp,
-            missing_progress: Self::INF_PROGRESS,
+            missing_progress: INF_PROGRESS,
             missing_quality: Quality::new(0),
             effects: self.effects.to_effects(),
             combo: self.combo,
@@ -75,8 +74,7 @@ impl ReducedState {
 #[derive(Debug)]
 pub struct FinishSolver {
     settings: Settings,
-    // maps ReducedState to maximum additional progress that can be gained from that state
-    memoization: HashMap<ReducedState, Progress>,
+    memoization: HashMap<ReducedState, Vec<Progress>>,
 }
 
 impl FinishSolver {
@@ -93,6 +91,8 @@ impl FinishSolver {
         }
         let mut finish_sequence: Vec<Action> = Vec::new();
         loop {
+            let mut best_time = 100000;
+            let mut best_action = Action::BasicSynthesis;
             for action in SEARCH_ACTIONS
                 .intersection(self.settings.allowed_actions)
                 .actions_iter()
@@ -100,31 +100,52 @@ impl FinishSolver {
                 let new_state = state.use_action(action, Condition::Normal, &self.settings);
                 match new_state {
                     State::InProgress(new_state) => {
-                        if self.can_finish(&new_state) {
-                            finish_sequence.push(action);
-                            state = new_state;
-                            break;
+                        let time = self.time_to_finish(&new_state);
+                        if time.is_some() && time.unwrap() + action.time_cost() < best_time {
+                            best_time = time.unwrap() + action.time_cost();
+                            best_action = action;
                         }
                     }
-                    State::Completed { missing_quality: _ } => {
-                        finish_sequence.push(action);
-                        return Some(finish_sequence);
+                    State::Completed { .. } => {
+                        if action.time_cost() < best_time {
+                            best_time = action.time_cost();
+                            best_action = action;
+                        }
                     }
                     _ => (),
                 }
+            }
+
+            finish_sequence.push(best_action);
+
+            let new_state = state.use_action(best_action, Condition::Normal, &self.settings);
+            match new_state {
+                State::InProgress(new_state) => state = new_state,
+                State::Completed { .. } => return Some(finish_sequence),
+                _ => (),
             }
         }
     }
 
     pub fn can_finish(&mut self, state: &InProgress) -> bool {
-        state.missing_progress <= self._do_solve(ReducedState::from_state(state))
+        *self.solve(ReducedState::from_state(state)).last().unwrap() >= state.missing_progress
     }
 
-    fn _do_solve(&mut self, state: ReducedState) -> Progress {
+    pub fn time_to_finish(&mut self, state: &InProgress) -> Option<u32> {
+        let result = self.solve(ReducedState::from_state(state));
+        for (time, progress) in result.into_iter().enumerate() {
+            if progress >= state.missing_progress {
+                return Some(time as u32);
+            }
+        }
+        None
+    }
+
+    fn solve(&mut self, state: ReducedState) -> Vec<Progress> {
         match self.memoization.get(&state) {
-            Some(progress) => *progress,
+            Some(result) => result.clone(),
             None => {
-                let mut max_progress = Progress::new(0);
+                let mut result = Vec::new();
                 for action in SEARCH_ACTIONS
                     .intersection(self.settings.allowed_actions)
                     .actions_iter()
@@ -135,31 +156,58 @@ impl FinishSolver {
                             .use_action(action, Condition::Normal, &self.settings);
                     match new_state {
                         State::InProgress(new_state) => {
-                            let gained_progress =
-                                ReducedState::INF_PROGRESS.sub(new_state.missing_progress);
-                            let new_state_potential =
-                                self._do_solve(ReducedState::from_state(&new_state));
-                            max_progress = std::cmp::max(
-                                max_progress,
-                                gained_progress.add(new_state_potential),
+                            let progress = INF_PROGRESS.sub(new_state.missing_progress);
+                            let sub_result = self.solve(ReducedState::from_state(&new_state));
+                            Self::update_result(
+                                &mut result,
+                                &sub_result,
+                                action.time_cost(),
+                                progress,
                             );
                         }
                         State::Failed { missing_progress } => {
-                            let gained_progress = ReducedState::INF_PROGRESS.sub(missing_progress);
-                            max_progress = std::cmp::max(max_progress, gained_progress);
+                            let progress = INF_PROGRESS.sub(missing_progress);
+                            Self::update_result(
+                                &mut result,
+                                &[Progress::new(0)],
+                                action.time_cost(),
+                                progress,
+                            );
                         }
-                        State::Completed { .. } => {
-                            unreachable!("INF_PROGRESS not high enough")
-                        }
-                        State::Invalid => (),
-                    }
-                    if max_progress >= self.settings.max_progress {
-                        break;
+                        _ => (),
                     }
                 }
-                self.memoization.insert(state, max_progress);
-                max_progress
+                self.canonicalize_result(&mut result);
+                self.memoization.insert(state, result.clone());
+                result
             }
+        }
+    }
+
+    fn update_result(
+        result: &mut Vec<Progress>,
+        sub_result: &[Progress],
+        time_offset: u32,
+        progress_increase: Progress,
+    ) {
+        if result.len() < sub_result.len() + time_offset as usize {
+            result.resize(sub_result.len() + time_offset as usize, Progress::new(0));
+        }
+        let result: &mut [Progress] =
+            &mut result[time_offset as usize..time_offset as usize + sub_result.len()];
+        for (a, b) in result.iter_mut().zip(sub_result.iter()) {
+            *a = std::cmp::max(*a, (*b).add(progress_increase));
+        }
+    }
+
+    fn canonicalize_result(&self, result: &mut Vec<Progress>) {
+        let mut rolling_max = Progress::new(0);
+        for a in result.iter_mut() {
+            rolling_max = std::cmp::max(rolling_max, *a);
+            *a = rolling_max;
+        }
+        while result.len() >= 2 && result[result.len() - 2] >= self.settings.max_progress {
+            result.pop();
         }
     }
 }
@@ -168,5 +216,55 @@ impl Drop for FinishSolver {
     fn drop(&mut self) {
         let finish_solver_states = self.memoization.len();
         dbg!(finish_solver_states);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn solve(settings: Settings, actions: &[Action]) -> Vec<Action> {
+        let state = State::new(&settings).use_actions(actions, Condition::Normal, &settings);
+        let result = FinishSolver::new(settings)
+            .get_finish_sequence(state.as_in_progress().unwrap())
+            .unwrap();
+        dbg!(&result);
+        result
+    }
+
+    #[test]
+    fn test_01() {
+        let settings = Settings {
+            max_cp: 100,
+            max_durability: 30,
+            max_progress: Progress::from(360.00),
+            max_quality: Quality::from(20000.00),
+            allowed_actions: ActionMask::from_level(90, true),
+        };
+        let actions = solve(settings, &[]);
+        assert_eq!(actions, [Action::Groundwork]);
+    }
+
+    #[test]
+    fn test_02() {
+        let settings = Settings {
+            max_cp: 100,
+            max_durability: 60,
+            max_progress: Progress::from(2100.00),
+            max_quality: Quality::from(20000.00),
+            allowed_actions: ActionMask::from_level(90, true),
+        };
+        let actions = solve(settings, &[]);
+        assert_eq!(
+            actions,
+            [
+                Action::MuscleMemory,
+                Action::Veneration,
+                Action::Groundwork,
+                Action::Groundwork,
+                Action::PrudentSynthesis,
+                Action::BasicSynthesis,
+            ]
+        );
     }
 }
