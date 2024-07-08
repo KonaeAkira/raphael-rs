4abf9110019e1eaa543aa3481afbe23a6b7fb546
KonaeAkira <longtruong2411@gmail.com>
2024-07-08 20:51:59 +0200

    Remove base url tag

--- build-web.sh
+++ build-web.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 export RANDOM_SUFFIX="-$(hexdump -vn8 -e'2/4 "%08x"' /dev/urandom)"
 export RUSTFLAGS="--cfg=web_sys_unstable_apis"
 
-trunk build index.html --public-url="https://www.raphael-xiv.com" --release --dist docs
+trunk build index.html --release --dist docs
 
 mv docs/webworker.js docs/webworker${RANDOM_SUFFIX}.js
 mv docs/webworker_bg.wasm docs/webworker${RANDOM_SUFFIX}_bg.wasm
