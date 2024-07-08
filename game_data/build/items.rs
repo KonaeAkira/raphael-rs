8e3053f2ba722ff7c4ffc7cbd4315b567978e011
KonaeAkira <longtruong2411@gmail.com>
2024-07-08 20:50:16 +0200

    Prune irrelevant items

--- game_data/build/items.rs
+++ game_data/build/items.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::Path;
@@ -5,9 +6,11 @@ use std::path::Path;
 use crate::records::ItemRecord;
 use crate::utils::read_csv_data;
 
-pub fn import_item_records() -> Result<(), Box<dyn std::error::Error>> {
+pub fn import_item_records(relevant_items: HashSet<u32>) -> Result<(), Box<dyn std::error::Error>> {
     let mut item_stats = phf_codegen::OrderedMap::new();
-    for item in read_csv_data::<ItemRecord>("data/en/Item.csv") {
+    for item in read_csv_data::<ItemRecord>("data/en/Item.csv")
+        .filter(|item| relevant_items.contains(&item.id))
+    {
         item_stats.entry(item.id, &format!(
             "Item {{ item_level: {item_level}, can_be_hq: {can_be_hq}, is_collectable: {is_collectable} }}",
             item_level = item.item_level,
@@ -19,17 +22,22 @@ pub fn import_item_records() -> Result<(), Box<dyn std::error::Error>> {
     let mut writer = BufWriter::new(File::create(out_path).unwrap());
     writeln!(writer, "{}", item_stats.build())?;
 
-    import_item_names("en")?;
-    import_item_names("de")?;
-    import_item_names("fr")?;
-    import_item_names("jp")?;
+    import_item_names(&relevant_items, "en")?;
+    import_item_names(&relevant_items, "de")?;
+    import_item_names(&relevant_items, "fr")?;
+    import_item_names(&relevant_items, "jp")?;
 
     Ok(())
 }
 
-fn import_item_names(lang: &'static str) -> Result<(), Box<dyn std::error::Error>> {
+fn import_item_names(
+    relevant_items: &HashSet<u32>,
+    lang: &'static str,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut item_names = phf_codegen::Map::new();
-    for item in read_csv_data::<ItemRecord>(format!("data/{}/Item.csv", lang)) {
+    for item in read_csv_data::<ItemRecord>(format!("data/{}/Item.csv", lang))
+        .filter(|item| relevant_items.contains(&item.id))
+    {
         item_names.entry(item.id, &format!("\"{}\"", item.name));
     }
     let out_path = Path::new(&std::env::var("OUT_DIR")?).join(format!("item_names_{}.rs", lang));
--- game_data/build/main.rs
+++ game_data/build/main.rs
@@ -1,5 +1,6 @@
 mod records;
 use records::*;
+use utils::read_csv_data;
 
 mod consumables;
 mod items;
@@ -18,10 +19,14 @@ fn main() {
 }
 
 fn import_game_data() -> Result<(), Box<dyn std::error::Error>> {
+    // keep track of relevant item IDs so that we don't serialize items that are never used
+    let mut relevant_items: HashSet<u32> = HashSet::new();
+
     let rlvls = import_rlvl_records()?;
-    consumables::import_consumable_records()?;
-    import_recipe_records(&rlvls)?;
-    items::import_item_records()?;
+    import_recipe_records(&mut relevant_items, &rlvls)?;
+
+    consumables::import_consumable_records(&mut relevant_items)?;
+    items::import_item_records(relevant_items)?;
     Ok(())
 }
 
@@ -42,7 +47,10 @@ fn import_rlvl_records() -> Result<Vec<RecipeLevelRecord>, Box<dyn std::error::E
     Ok(rlvl_records)
 }
 
-fn import_recipe_records(rlvls: &[RecipeLevelRecord]) -> Result<(), Box<dyn std::error::Error>> {
+fn import_recipe_records(
+    relevant_items: &mut HashSet<u32>,
+    rlvls: &[RecipeLevelRecord],
+) -> Result<(), Box<dyn std::error::Error>> {
     fn apply_factor(base: u32, factor: u32) -> u32 {
         ((base * factor) as f64 / 100.0).floor() as u32
     }
@@ -50,10 +58,7 @@ fn import_recipe_records(rlvls: &[RecipeLevelRecord]) -> Result<(), Box<dyn std:
     let mut items_with_recipe = HashSet::new();
     let mut recipes = phf_codegen::OrderedMap::new();
 
-    let mut recipes_csv = csv::Reader::from_path("data/Recipe.csv")?;
-    for recipe_record in recipes_csv.deserialize::<RecipeRecord>() {
-        let recipe_record = recipe_record?;
-
+    for recipe_record in read_csv_data::<RecipeRecord>("data/Recipe.csv") {
         // skip if a recipe for this item already exists
         // might be a problem if an item has multiple recipes with different ingredients
         if items_with_recipe.contains(&recipe_record.resulting_item) {
@@ -64,6 +69,14 @@ fn import_recipe_records(rlvls: &[RecipeLevelRecord]) -> Result<(), Box<dyn std:
         if recipe_record.resulting_item == 0 {
             continue;
         }
+
+        relevant_items.insert(recipe_record.resulting_item);
+        relevant_items.insert(recipe_record.ingredient_id_0);
+        relevant_items.insert(recipe_record.ingredient_id_1);
+        relevant_items.insert(recipe_record.ingredient_id_2);
+        relevant_items.insert(recipe_record.ingredient_id_3);
+        relevant_items.insert(recipe_record.ingredient_id_4);
+        relevant_items.insert(recipe_record.ingredient_id_5);
 
         let ingredients = format!(
                 "[Ingredient {{ item_id: {}, amount: {} }}, Ingredient {{ item_id: {}, amount: {} }}, Ingredient {{ item_id: {}, amount: {} }}, Ingredient {{ item_id: {}, amount: {} }}, Ingredient {{ item_id: {}, amount: {} }}, Ingredient {{ item_id: {}, amount: {} }}]",
