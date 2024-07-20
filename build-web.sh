1b7a931305e8da2ddb30cafa89cb7e7ea8e18fc2
KonaeAkira <longtruong2411@gmail.com>
2024-07-20 14:35:15 +0200

    Add script to install dependencies and build - Removed the build directory

--- build-web.sh
+++ build-web.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-export RANDOM_SUFFIX="-$(hexdump -vn8 -e'2/4 "%08x"' /dev/urandom)"
+export RANDOM_SUFFIX="-$(echo $RANDOM$RANDOM | md5sum | head -c 8)"
 export RUSTFLAGS="--cfg=web_sys_unstable_apis"
 
-trunk build index.html --release --dist docs
+trunk build index.html --release --dist distrib
 
-mv docs/webworker.js docs/webworker${RANDOM_SUFFIX}.js
-mv docs/webworker_bg.wasm docs/webworker${RANDOM_SUFFIX}_bg.wasm
+mv distrib/webworker.js distrib/webworker${RANDOM_SUFFIX}.js
+mv distrib/webworker_bg.wasm distrib/webworker${RANDOM_SUFFIX}_bg.wasm
