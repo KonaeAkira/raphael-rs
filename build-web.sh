0c9962513370b17caae4c1f91fbc34fac8e4cabf
KonaeAkira <longtruong2411@gmail.com>
2024-07-20 18:40:36 +0200

    Use branch name instead of commit hash for base URL

--- build-web.sh
+++ build-web.sh
@@ -6,8 +6,8 @@ export RANDOM_SUFFIX="-$(echo $RANDOM$RANDOM | md5sum | head -c 8)"
 export RUSTFLAGS="--cfg=web_sys_unstable_apis"
 
 if [ ! -v BASE_URL ]; then
-    COMMIT=$(git rev-parse --short HEAD)
-    export BASE_URL="https://$COMMIT.raphael-rs.pages.dev"
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    export BASE_URL="https://${BRANCH//./-}.raphael-rs.pages.dev"
 fi
 
 trunk build index.html --release --dist distrib
