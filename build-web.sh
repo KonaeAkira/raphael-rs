3548b751a7d140602553536371a1c3542a1a4158
KonaeAkira <longtruong2411@gmail.com>
2024-07-20 18:25:38 +0200

    Fix unbound variable error

--- build-web.sh
+++ build-web.sh
@@ -5,10 +5,9 @@ set -euxo pipefail
 export RANDOM_SUFFIX="-$(echo $RANDOM$RANDOM | md5sum | head -c 8)"
 export RUSTFLAGS="--cfg=web_sys_unstable_apis"
 
-if [[ -z "${BASE_URL}" ]]; then
-    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-    BASE_URL="https://${BRANCH_NAME//./-}.raphael-rs.pages.dev"
-    export BASE_URL
+if [ ! -v BASE_URL ]; then
+    COMMIT=$(git rev-parse --short HEAD)
+    export BASE_URL="https://$COMMIT.raphael-rs.pages.dev"
 fi
 
 trunk build index.html --release --dist distrib
