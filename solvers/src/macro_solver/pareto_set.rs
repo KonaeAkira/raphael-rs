5c93ca27ab230677abe207a0356ab95b862394f1
KonaeAkira <longtruong2411@gmail.com>
2024-07-27 19:44:07 +0200

    Remove progress-based pareto front

--- solvers/src/macro_solver/pareto_set.rs
+++ solvers/src/macro_solver/pareto_set.rs
@@ -3,13 +3,13 @@ use rustc_hash::FxHashMap;
 use simulator::{ComboAction, Effects, Settings, SimulationState};
 
 #[derive(Clone, Copy, PartialEq, Eq)]
-struct GenericValue {
+struct Value {
     cp: i16,
     quality: [u16; 2],
     inner_quiet: u8,
 }
 
-impl GenericValue {
+impl Value {
     pub fn new(state: SimulationState) -> Self {
         Self {
             cp: state.cp,
@@ -19,7 +19,7 @@ impl GenericValue {
     }
 }
 
-impl Dominate for GenericValue {
+impl Dominate for Value {
     fn dominate(&self, other: &Self) -> bool {
         self.cp >= other.cp
             && self.quality[0] >= other.quality[0]
@@ -29,67 +29,30 @@ impl Dominate for GenericValue {
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-struct GenericKey {
+struct Key {
     durability: i8,
     missing_progress: u16,
     effects: Effects,
     combo: Option<ComboAction>,
 }
 
-impl GenericKey {
-    pub fn new(state: SimulationState) -> Self {
-        Self {
-            durability: state.durability,
-            missing_progress: state.missing_progress,
-            effects: state.effects.with_inner_quiet(0), // iq is included in the pareto value
-            combo: state.combo,
-        }
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-struct ProgressValue {
-    cp: i16,
-    quality: u16,
-    missing_progress: u16,
-}
-
-impl ProgressValue {
-    pub fn new(state: SimulationState) -> Self {
-        Self {
-            cp: state.cp,
-            quality: state.get_quality(),
-            missing_progress: state.missing_progress,
-        }
-    }
-}
-
-impl Dominate for ProgressValue {
-    fn dominate(&self, other: &Self) -> bool {
-        self.cp >= other.cp
-            && self.quality >= other.quality
-            && self.missing_progress <= other.missing_progress
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-struct ProgressKey {
-    durability: i8,
-    effects: Effects,
-    combo: Option<ComboAction>,
-}
-
-impl ProgressKey {
-    pub fn new(state: SimulationState) -> Self {
-        Self {
-            durability: state.durability,
-            effects: state
+impl Key {
+    pub fn new(state: SimulationState, settings: &Settings) -> Self {
+        let effects = if state.get_quality() >= settings.max_quality {
+            state
                 .effects
                 .with_inner_quiet(0)
                 .with_innovation(0)
                 .with_great_strides(0)
+                .with_guard(0)
                 .with_quick_innovation_used(true)
-                .with_guard(0),
+        } else {
+            state.effects.with_inner_quiet(0) // iq is included in the pareto value
+        };
+        Self {
+            durability: state.durability,
+            missing_progress: state.missing_progress,
+            effects,
             combo: state.combo,
         }
     }
@@ -97,53 +60,32 @@ impl ProgressKey {
 
 #[derive(Default)]
 pub struct ParetoSet {
-    generic_buckets: FxHashMap<GenericKey, ParetoFront<GenericValue>>,
-    progress_buckets: FxHashMap<ProgressKey, ParetoFront<ProgressValue>>,
+    buckets: FxHashMap<Key, ParetoFront<Value>>,
 }
 
 impl ParetoSet {
     pub fn insert(&mut self, state: SimulationState, settings: &Settings) -> bool {
-        if state.get_quality() < settings.max_quality {
-            self.generic_buckets
-                .entry(GenericKey::new(state))
-                .or_default()
-                .push(GenericValue::new(state))
-        } else {
-            self.progress_buckets
-                .entry(ProgressKey::new(state))
-                .or_default()
-                .push(ProgressValue::new(state))
-        }
+        self.buckets
+            .entry(Key::new(state, settings))
+            .or_default()
+            .push(Value::new(state))
     }
 
     pub fn contains(&self, state: SimulationState, settings: &Settings) -> bool {
-        if state.get_quality() < settings.max_quality {
-            match self.generic_buckets.get(&GenericKey::new(state)) {
-                Some(pareto_front) => pareto_front.as_slice().contains(&GenericValue::new(state)),
-                None => false,
-            }
-        } else {
-            match self.progress_buckets.get(&ProgressKey::new(state)) {
-                Some(pareto_front) => pareto_front.as_slice().contains(&ProgressValue::new(state)),
-                None => false,
-            }
+        match self.buckets.get(&Key::new(state, settings)) {
+            Some(pareto_front) => pareto_front.as_slice().contains(&Value::new(state)),
+            None => false,
         }
     }
 }
 
 // impl Drop for ParetoSet {
 //     fn drop(&mut self) {
-//         let generic_pareto_entries: usize = self
+//         let pareto_entries: usize = self
 //             .generic_buckets
 //             .iter()
 //             .map(|(_key, value)| value.len())
 //             .sum();
-//         dbg!(self.generic_buckets.len(), generic_pareto_entries);
-//         let progress_pareto_entries: usize = self
-//             .progress_buckets
-//             .iter()
-//             .map(|(_key, value)| value.len())
-//             .sum();
-//         dbg!(self.progress_buckets.len(), progress_pareto_entries);
+//         dbg!(self.buckets.len(), pareto_entries);
 //     }
 // }
