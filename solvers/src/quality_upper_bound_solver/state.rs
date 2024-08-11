61345e6fd92bbaeea63af2654e58c27bb092a95f
KonaeAkira <longtruong2411@gmail.com>
2024-08-11 10:11:46 +0200

    Compress GreatStrides state-space

--- solvers/src/quality_upper_bound_solver/state.rs
+++ solvers/src/quality_upper_bound_solver/state.rs
@@ -18,11 +18,13 @@ impl ReducedState {
     pub fn from_state(state: SimulationState, durability_cost: i16) -> Self {
         let used_durability = (i8::MAX - state.durability) / 5;
         let cp = state.cp - used_durability as i16 * durability_cost;
+        let great_strides_active = state.effects.great_strides() != 0;
         Self {
             cp,
             combo: state.combo,
             effects: state
                 .effects
+                .with_great_strides(if great_strides_active { 3 } else { 0 })
                 .with_trained_perfection(SingleUse::Unavailable)
                 .with_manipulation(0)
                 .with_guard(1),
