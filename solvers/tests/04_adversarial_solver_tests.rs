92227613f613d751d43cf0e470fc37c0caa82e49
KonaeAkira <longtruong2411@gmail.com>
2024-08-31 18:40:48 +0200

    Add more adversarial solver tests

--- solvers/tests/04_adversarial_solver_tests.rs
+++ solvers/tests/04_adversarial_solver_tests.rs
@@ -214,3 +214,71 @@ fn stuffed_peppers() {
     let score = get_score_triple(&settings, &actions);
     assert_eq!(score, (11682, 16, 45));
 }
+
+#[test]
+fn test_rare_tacos_2() {
+    // lv100 Rarefied Tacos de Carne Asada
+    // 4785 CMS, 4758 Ctrl, 646 CP
+    let settings = Settings {
+        max_cp: 646,
+        max_durability: 80,
+        max_progress: 6600,
+        max_quality: 12000,
+        base_progress: 256,
+        base_quality: 265,
+        job_level: 100,
+        allowed_actions: ActionMask::all()
+            .remove(Action::TrainedEye)
+            .remove(Action::HeartAndSoul)
+            .remove(Action::QuickInnovation),
+        adversarial: true,
+    };
+    let actions = solve(&settings, false).unwrap();
+    let score = get_score_triple(&settings, &actions);
+    assert_eq!(score, (12138, 32, 91));
+}
+
+#[test]
+fn test_mountain_chromite_ingot_no_manipulation() {
+    // Mountain Chromite Ingot
+    // 3076 Craftsmanship, 3106 Control, Level 90, HQ Tsai Tou Vonou
+    let settings = Settings {
+        max_cp: 616,
+        max_durability: 40,
+        max_progress: 2000,
+        max_quality: 8200,
+        base_progress: 217,
+        base_quality: 293,
+        job_level: 90,
+        allowed_actions: ActionMask::from_level(90)
+            .remove(Action::Manipulation)
+            .remove(Action::TrainedEye)
+            .remove(Action::HeartAndSoul)
+            .remove(Action::QuickInnovation),
+        adversarial: true,
+    };
+    let actions = solve(&settings, true).unwrap();
+    let score = get_score_triple(&settings, &actions);
+    assert_eq!(score, (8232, 14, 38));
+}
+
+#[test]
+fn test_indagator_3858_4057() {
+    let settings = Settings {
+        max_cp: 687,
+        max_durability: 70,
+        max_progress: 5720,
+        max_quality: 12900,
+        base_progress: 239,
+        base_quality: 271,
+        job_level: 90,
+        allowed_actions: ActionMask::from_level(90)
+            .remove(Action::TrainedEye)
+            .remove(Action::HeartAndSoul)
+            .remove(Action::QuickInnovation),
+        adversarial: true,
+    };
+    let actions = solve(&settings, false).unwrap();
+    let score = get_score_triple(&settings, &actions);
+    assert_eq!(score, (10686, 26, 71));
+}
