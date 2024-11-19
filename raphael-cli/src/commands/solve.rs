2b40a31c1f834b1ea0bd5de7b79c658e63a8380e
augenfrosch <augenfrosch@okonnek.name>
2024-11-19 18:28:10 +0100

    Add food & potion selection to cli

--- raphael-cli/src/commands/solve.rs
+++ raphael-cli/src/commands/solve.rs
@@ -1,5 +1,5 @@
 use clap::Args;
-use game_data::{get_game_settings, CrafterStats, RECIPES};
+use game_data::{get_game_settings, CrafterStats, MEALS, POTIONS, RECIPES};
 use simulator::SimulationState;
 use solvers::MacroSolver;
 
@@ -24,6 +24,14 @@ pub struct SolveArgs {
     /// Crafter level
     #[arg(short, long, default_value_t = 100)]
     pub level: u8,
+
+    /// Food to use, in the format '<ITEM_ID>[,HQ]'
+    #[arg(long, value_parser = parse_consumable)]
+    pub food: Option<ConsumableArg>,
+
+    /// Potion to use, in the format '<ITEM_ID>[,HQ]'
+    #[arg(long, value_parser = parse_consumable)]
+    pub potion: Option<ConsumableArg>,
 
     /// Enable Manipulation
     #[arg(short, long, default_value_t = false)]
@@ -50,13 +58,95 @@ pub struct SolveArgs {
     pub unsound: bool,
 }
 
+fn parse_consumable(s: &str) -> Result<ConsumableArg, String> {
+    const PARSE_ERROR_STRING: &'static str =
+        "Consumable is not parsable. Consumables must have the format '<ITEM_ID>[,HQ]'";
+    let segments: Vec<&str> = s.split(",").collect();
+    let item_id_str = segments.get(0);
+    let item_id: u32;
+    match item_id_str {
+        Some(&str) => item_id = str.parse().map_err(|_| PARSE_ERROR_STRING.to_owned())?,
+        None => return Err(PARSE_ERROR_STRING.to_owned()),
+    }
+    match segments.len() {
+        1 => Ok(ConsumableArg::NQ(item_id)),
+        2 => {
+            let hq_str = segments.get(1).unwrap().to_owned();
+            match hq_str {
+                "HQ" => Ok(ConsumableArg::HQ(item_id)),
+                _ => Err(PARSE_ERROR_STRING.to_owned()),
+            }
+        }
+        _ => Err(PARSE_ERROR_STRING.to_owned()),
+    }
+}
 
+#[derive(Clone, Copy, Debug)]
+pub enum ConsumableArg {
+    /// NQ Consumable
+    NQ(u32),
+    /// HQ Consumable
+    HQ(u32),
+}
 
 pub fn execute(args: &SolveArgs) {
     let recipe = RECIPES
         .iter()
         .find(|r| r.item_id == args.item_id)
-        .expect("Recipe not found");
+        .expect(&format!(
+            "Unable to find Recipe for an item with item ID: {}",
+            args.item_id
+        ));
+    let food = match args.food {
+        Some(food_arg) => {
+            let item_id;
+            let is_hq;
+            match food_arg {
+                ConsumableArg::NQ(id) => {
+                    item_id = id;
+                    is_hq = false;
+                }
+                ConsumableArg::HQ(id) => {
+                    item_id = id;
+                    is_hq = true;
+                }
+            };
+
+            Some(
+                MEALS
+                    .iter()
+                    .find(|m| (m.item_id == item_id) && (m.hq == is_hq))
+                    .expect(&format!("Unable to find Food with item ID: {item_id}"))
+                    .to_owned(),
+            )
+        }
+        None => None,
+    };
+    let potion = match args.potion {
+        Some(potion_arg) => {
+            let item_id;
+            let is_hq;
+            match potion_arg {
+                ConsumableArg::NQ(id) => {
+                    item_id = id;
+                    is_hq = false;
+                }
+                ConsumableArg::HQ(id) => {
+                    item_id = id;
+                    is_hq = true;
+                }
+            };
+
+            Some(
+                POTIONS
+                    .iter()
+                    .find(|m| (m.item_id == item_id) && (m.hq == is_hq))
+                    .expect(&format!("Unable to find Potion with item ID: {item_id}"))
+                    .to_owned(),
+            )
+        }
+        None => None,
+    };
 
     let crafter_stats = CrafterStats {
         craftsmanship: args.craftsmanship,
@@ -68,7 +158,7 @@ pub fn execute(args: &SolveArgs) {
         quick_innovation: args.quick_innovation,
     };
 
-    let settings = get_game_settings(*recipe, crafter_stats, None, None, args.adversarial);
+    let settings = get_game_settings(*recipe, crafter_stats, food, potion, args.adversarial);
     let state = SimulationState::new(&settings);
 
     let mut solver = MacroSolver::new(
