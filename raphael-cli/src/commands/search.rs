30f60dce0d6523a2ee1ff35f69eed92387c2706e
augenfrosch <augenfrosch@okonnek.name>
2024-11-22 19:02:20 +0100

    Add language option to search

--- raphael-cli/src/commands/search.rs
+++ raphael-cli/src/commands/search.rs
@@ -9,10 +9,34 @@ pub struct SearchArgs {
     /// The delimiter the output uses between fields
     #[arg(long, alias = "OFS", default_value = " ", env = "OFS")]
     output_field_separator: String,
+
+    /// The language the input pattern and output use
+    #[arg(short, long, alias = "locale", value_enum, ignore_case = true, default_value_t = SearchLanguage::EN)]
+    language: SearchLanguage,
+}
+
+#[derive(Copy, Clone, ValueEnum, Debug)]
+pub enum SearchLanguage {
+    EN,
+    DE,
+    FR,
+    JP,
+}
+
+impl Into::<Locale> for SearchLanguage {
+    fn into(self) -> Locale {
+       match self {
+            SearchLanguage::EN => Locale::EN,
+            SearchLanguage::DE => Locale::DE,
+            SearchLanguage::FR => Locale::FR,
+            SearchLanguage::JP => Locale::JP,
+        }
+    }
 }
 
 pub fn execute(args: &SearchArgs) {
-    let matches = game_data::find_recipes(&args.pattern, Locale::EN);
+    let locale = args.language.into();
+    matches = game_data::find_recipes(&args.pattern, locale);
     if matches.is_empty() {
         println!("No matches found");
         return;
@@ -20,7 +44,7 @@ pub fn execute(args: &SearchArgs) {
 
     for recipe_idx in matches {
         let recipe = &RECIPES[recipe_idx];
-        let name = get_item_name(recipe.item_id, false, Locale::EN);
+        let name = get_item_name(recipe.item_id, false, locale);
         println!(
             "{item_id}{separator}{name}",
             item_id = recipe.item_id,
