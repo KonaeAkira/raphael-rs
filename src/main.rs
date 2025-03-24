2b3f3bc95acc02a4269340bad0c738c5018b6d57
KonaeAkira <longtruong2411@gmail.com>
2025-03-24 19:05:19 +0100

    Add spinner while waiting for app to start

--- src/main.rs
+++ src/main.rs
@@ -22,29 +22,38 @@ fn main() -> eframe::Result<()> {
 }
 
 #[cfg(target_arch = "wasm32")]
-fn get_canvas() -> Option<web_sys::HtmlCanvasElement> {
-    use web_sys::wasm_bindgen::JsCast;
-    let document = web_sys::window()?.document()?;
-    let canvas = document.get_element_by_id("the_canvas_id")?;
-    canvas.dyn_into::<web_sys::HtmlCanvasElement>().ok()
-}
-
-#[cfg(target_arch = "wasm32")]
 fn main() {
     // Redirect `log` message to `console.log` and friends:
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 
+    fn get_canvas() -> Option<web_sys::HtmlCanvasElement> {
+        use web_sys::wasm_bindgen::JsCast;
+        let document = web_sys::window()?.document()?;
+        let canvas = document.get_element_by_id("the_canvas_id")?;
+        canvas.dyn_into::<web_sys::HtmlCanvasElement>().ok()
+    }
+
+    fn remove_loading_spinner() -> Option<()> {
+        let document = web_sys::window()?.document()?;
+        let spinner = document.get_element_by_id("spinner")?;
+        spinner.remove();
+        Some(())
+    }
+
     wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
+        let start_result = eframe::WebRunner::new()
             .start(
-                get_canvas().expect("Failed to get HTML canvas"),
+                get_canvas().unwrap(),
                 eframe::WebOptions::default(),
                 Box::new(|cc| {
                     egui_extras::install_image_loaders(&cc.egui_ctx);
                     Ok(Box::new(raphael_xiv::MacroSolverApp::new(cc)))
                 }),
             )
-            .await
-            .expect("failed to start eframe");
+            .await;
+        remove_loading_spinner();
+        if let Err(error) = start_result {
+            panic!("Failed to start eframe: {error:?}");
+        }
     });
 }
