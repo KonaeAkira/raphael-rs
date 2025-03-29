38290a041c526502273b2d86de0fb5fb733c08f7
KonaeAkira <longtruong2411@gmail.com>
2025-03-29 21:21:09 +0100

    Remove synth begin combo handling from inner solvers

--- raphael-solver/src/quality_upper_bound_solver/solver.rs
+++ raphael-solver/src/quality_upper_bound_solver/solver.rs
@@ -76,9 +76,6 @@ impl QualityUpperBoundSolver {
     /// Returns an upper-bound on the maximum Quality achievable from this state while also maxing out Progress.
     /// There is no guarantee on the tightness of the upper-bound.
     pub fn quality_upper_bound(&mut self, state: SimulationState) -> Result<u16, SolverException> {
-        if state.combo == Combo::SynthesisBegin {
-            return Ok(self.settings.simulator_settings.max_quality);
-        }
         if state.combo != Combo::None {
             return Err(SolverException::InternalError(format!(
                 "\"{:?}\" combo in quality upper bound solver",
