785507d0115eaabc377c735bcc29e93c5cf82191
augenfrosch <augenfrosch@okonnek.name>
2025-05-01 03:52:44 +0200

    Remove commented out rustflags commands & move comment about them

--- serve-web.sh
+++ serve-web.sh
@@ -2,7 +2,6 @@
 
 set -exo pipefail
 
-#export RUSTFLAGS="--cfg=web_sys_unstable_apis -Ctarget-feature=+atomics,+bulk-memory -Clink-arg=--max-memory=4294967296"
 export BASE_URL="http://localhost:8080"
 
 # Requires nightly toolchain, rust-src component, and wasm32-unknown-unkown target
