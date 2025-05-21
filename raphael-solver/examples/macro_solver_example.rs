7094924fe211846612d4982d1e0bf30857ad47a2
KonaeAkira <longtruong2411@gmail.com>
2025-05-21 21:37:25 +0200

    Revert macro solver example

--- raphael-solver/examples/macro_solver_example.rs
+++ raphael-solver/examples/macro_solver_example.rs
@@ -7,19 +7,18 @@ fn main() {
         .format_target(false)
         .init();
 
-    // lv99 Rarefied Stuffed Peppers
-    // 4785 CMS, 4758 Ctrl, 646 CP
     let simulator_settings = Settings {
-        max_cp: 646,
-        max_durability: 80,
-        max_progress: 6300,
-        max_quality: 40000,
-        base_progress: 289,
-        base_quality: 360,
+        max_cp: 753,
+        max_durability: 20,
+        max_progress: 4700,
+        max_quality: 14900,
+        base_progress: 310,
+        base_quality: 324,
         job_level: 100,
         allowed_actions: ActionMask::all()
             .remove(Action::TrainedEye)
-            .remove(Action::HeartAndSoul),
+            .remove(Action::HeartAndSoul)
+            .remove(Action::QuickInnovation),
         adversarial: false,
         backload_progress: false,
     };
