32d9407825cc0af63a6f8a6d966a9c138604d3cd
augenfrosch <augenfrosch@okonnek.name>
2025-05-24 19:34:55 +0200

    Allow for frame time to be shown in graph

--- src/widgets/render_info.rs
+++ src/widgets/render_info.rs
@@ -12,23 +12,30 @@ enum RunMode {
 pub struct RenderInfoState {
     run_mode: RunMode,
     cpu_usage_history: History<f32>,
+    frame_time_history: History<f32>,
     last_frames_time: Option<f64>,
+
+    y_lim: f32,
+    plot_cpu_usage: bool,
+    plot_frame_time: bool,
 }
 
 impl RenderInfoState {
     pub fn update(&mut self, ctx: &egui::Context, frame: &eframe::Frame) {
+        let now = ctx.input(|i| i.time);
         if let Some(cpu_usage) = frame.info().cpu_usage {
-            self.cpu_usage_history
-                .add(self.last_frames_time.unwrap_or_default(), cpu_usage);
+            let last_frames_time = self.last_frames_time.unwrap_or_default();
+            self.cpu_usage_history.add(last_frames_time, cpu_usage);
+            self.frame_time_history
+                .add(last_frames_time, (now - last_frames_time) as f32);
         }
-        self.last_frames_time = Some(ctx.input(|i| i.time));
+        self.last_frames_time = Some(now);
 
         match self.run_mode {
             RunMode::Continuous => {
                 ctx.request_repaint();
             }
-            RunMode::Reactive => {
-            }
+            RunMode::Reactive => {}
         }
     }
 
@@ -45,7 +52,9 @@ impl RenderInfoState {
     }
 
     pub fn mean_frame_time(&self) -> f32 {
-        self.cpu_usage_history.mean_time_interval().unwrap_or_default()
+        self.cpu_usage_history
+            .mean_time_interval()
+            .unwrap_or_default()
     }
 
     pub fn average_fps(&self) -> f32 {
@@ -55,8 +64,6 @@ impl RenderInfoState {
             0.0
         }
     }
-
-    
 }
 
 impl Default for RenderInfoState {
@@ -64,7 +71,12 @@ impl Default for RenderInfoState {
         Self {
             run_mode: RunMode::Reactive,
             cpu_usage_history: History::new(0..300, 1.0),
+            frame_time_history: History::new(0..300, 1.0),
             last_frames_time: None,
+
+            y_lim: 0.010,
+            plot_cpu_usage: true,
+            plot_frame_time: false,
         }
     }
 }
@@ -125,7 +137,22 @@ impl<'a> RenderInfo<'a> {
 
             ui.end_row();
         });
-        ui.collapsing("📊 CPU usage history", |ui| {
+        ui.collapsing("📊 History", |ui| {
+            ui.horizontal(|ui| {
+                ui.checkbox(&mut self.state.plot_cpu_usage, "CPU usage");
+                ui.checkbox(&mut self.state.plot_frame_time, "Frame time");
+            });
+            ui.horizontal(|ui| {
+                ui.label("Y axis limit:");
+                let mut y_lim = self.state.y_lim * 1000.0;
+                ui.add(
+                    egui::DragValue::new(&mut y_lim)
+                        .range(1.0..=30.0)
+                        .suffix(" ms"),
+                );
+                self.state.y_lim = y_lim * 0.001;
+            });
+
             self.graph(ui);
         });
 
@@ -184,64 +211,55 @@ impl<'a> RenderInfo<'a> {
     }
 
     fn graph(&self, ui: &mut egui::Ui) -> egui::Response {
-        use egui::{Pos2, Rect, Sense, Shape, Stroke, TextStyle, emath, epaint, pos2, vec2};
+        use egui::{Rect, Sense, Shape, Stroke, TextStyle, emath, epaint, pos2, vec2};
 
-        let history = &self.state.cpu_usage_history;
+        let cpu_usage_history = &self.state.cpu_usage_history;
 
         let size = vec2(ui.available_size_before_wrap().x, 100.0);
         let (rect, response) = ui.allocate_at_least(size, Sense::hover());
         let style = ui.style().noninteractive();
 
-        let graph_top_cpu_usage = 0.010; // 10.0 ms
-        let graph_rect = Rect::from_x_y_ranges(history.max_age()..=0.0, graph_top_cpu_usage..=0.0);
+        let graph_top_cpu_usage = self.state.y_lim; // 10.0 ms
+        let graph_rect =
+            Rect::from_x_y_ranges(cpu_usage_history.max_age()..=0.0, graph_top_cpu_usage..=0.0);
         let to_screen = emath::RectTransform::from_to(graph_rect, rect);
 
-        let mut shapes = Vec::with_capacity(3 + history.len()); // Preallocates space for box + hover line & text + samples
+        // Preallocates space for box + hover line & text + samples
+        let number_of_shapes = 3 + 2 * cpu_usage_history.len();
+        let mut shapes = Vec::with_capacity(number_of_shapes);
         shapes.push(Shape::Rect(epaint::RectShape::new(
             rect,
             style.corner_radius,
             ui.visuals().extreme_bg_color,
             ui.style().noninteractive().bg_stroke,
-            egui::StrokeKind::Inside,
+            egui::StrokeKind::Middle,
         )));
 
-        let rect = rect.shrink(4.0);
-        let color = ui.visuals().text_color();
-        let line_stroke = Stroke::new(1.0, color);
-
-        let right_side_time = self.state.last_frames_time.unwrap_or_default();
-
-        // Add samples
-        let mut history_peekable_iter = history.iter().peekable();
-        for _ in 0..history.len() {
-            if let Some((time, cpu_usage)) = history_peekable_iter.next() {
-                let age = (right_side_time - time) as f32;
-                let pos = to_screen.transform_pos_clamped(Pos2::new(age, cpu_usage));
-
-                let mut draw_disconnected = true;
-                if let Some((following_time, following_cpu_usage)) = history_peekable_iter.peek() {
-                    let time_distance = following_time - time;
-                    // connect samples with line if they are in a window of rougly one frame at 60 fps
-                    // rounded and with additional ~1ms, since normal debug builds can miss this window quite often
-                    if time_distance < 0.018 {
-                        let following_age = (right_side_time - following_time) as f32;
-                        let following_pos = to_screen
-                            .transform_pos_clamped(Pos2::new(following_age, *following_cpu_usage));
-
-                        shapes.push(Shape::line_segment([pos, following_pos], line_stroke));
-                        draw_disconnected = false;
-                    }
-                }
-
-                if draw_disconnected {
-                    shapes.push(Shape::circle_filled(pos, 1.0, color));
-                }
-            } else {
-                break;
-            }
+        if self.state.plot_cpu_usage {
+            let color = ui.visuals().text_color();
+            let line_stroke = Stroke::new(1.0, color);
+            self.add_samples(
+                &mut shapes,
+                cpu_usage_history,
+                &to_screen,
+                line_stroke,
+                true,
+            );
+        }
+        if self.state.plot_frame_time {
+            let color = ui.visuals().weak_text_color();
+            let line_stroke = Stroke::new(1.0, color);
+            self.add_samples(
+                &mut shapes,
+                &self.state.frame_time_history,
+                &to_screen,
+                line_stroke,
+                false,
+            );
         }
 
         // Add hover line & text
+        let rect = rect.shrink(4.0);
         let color = ui.visuals().strong_text_color();
         let line_stroke = Stroke::new(1.0, color);
         if let Some(pointer_pos) = response.hover_pos() {
@@ -267,5 +285,49 @@ impl<'a> RenderInfo<'a> {
         ui.painter().extend(shapes);
 
         response
+    }
+
+    fn add_samples(
+        &self,
+        shapes: &mut Vec<egui::Shape>,
+        history: &History<f32>,
+        to_screen: &egui::emath::RectTransform,
+        line_stroke: egui::Stroke,
+        assume_value_continuity: bool,
+    ) {
+        let right_side_time = self.state.last_frames_time.unwrap_or_default();
+        let mut history_peekable_iter = history.iter().peekable();
+        for _ in 0..history.len() {
+            if let Some((time, value)) = history_peekable_iter.next() {
+                let age = (right_side_time - time) as f32;
+                let pos = to_screen.transform_pos_clamped(egui::Pos2::new(age, value));
+
+                let mut draw_disconnected = true;
+                if let Some((following_time, following_value)) = history_peekable_iter.peek() {
+                    let time_distance = following_time - time;
+                    let value_difference = following_value - value;
+                    // connect samples with line if they are in a window of rougly one frame at 60 fps
+                    // rounded and with additional ~1ms, since normal debug builds can miss this window quite often
+                    if time_distance < 0.018
+                        && (assume_value_continuity || value_difference.abs() < 0.020)
+                    {
+                        let following_age = (right_side_time - following_time) as f32;
+                        let following_pos = to_screen.transform_pos_clamped(egui::Pos2::new(
+                            following_age,
+                            *following_value,
+                        ));
+
+                        shapes.push(egui::Shape::line_segment([pos, following_pos], line_stroke));
+                        draw_disconnected = false;
+                    }
+                }
+
+                if draw_disconnected {
+                    shapes.push(egui::Shape::circle_filled(pos, 1.0, line_stroke.color));
+                }
+            } else {
+                break;
+            }
+        }
     }
 }
