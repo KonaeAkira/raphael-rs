be2dcce7db92741f882fdfd32f91b813c27fec8f
KonaeAkira <longtruong2411@gmail.com>
2025-08-29 14:36:48 +0200

    Fix wrong iq quality lut for adversarial mode

--- raphael-solver/src/step_lower_bound_solver/solver.rs
+++ raphael-solver/src/step_lower_bound_solver/solver.rs
@@ -39,6 +39,7 @@ pub struct StepLbSolver {
 
 impl StepLbSolver {
     pub fn new(mut settings: SolverSettings, interrupt_signal: utils::AtomicFlag) -> Self {
+        let iq_quality_lut = utils::compute_iq_quality_lut(&settings);
         settings.simulator_settings.adversarial = false;
         ReducedState::optimize_action_mask(&mut settings.simulator_settings);
         Self {
@@ -52,7 +53,7 @@ impl StepLbSolver {
             precompute_templates: Self::generate_precompute_templates(&settings),
             next_precompute_step_budget: NonZeroU8::new(1).unwrap(),
             precomputed_states: 0,
-            iq_quality_lut: utils::compute_iq_quality_lut(&settings),
+            iq_quality_lut,
             largest_progress_increase: largest_single_action_progress_increase(&settings),
         }
     }
