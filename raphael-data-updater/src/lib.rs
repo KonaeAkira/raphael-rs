04048d56afaa9846fd74e655dc43f26b0c7deaeb
augenfrosch <45207902+augenfrosch@users.noreply.github.com>
2025-08-31 12:13:25 +0200

    Add searching for recipes by stellar missions (#206)

--- raphael-data-updater/src/lib.rs
+++ raphael-data-updater/src/lib.rs
@@ -13,6 +13,9 @@ pub use item::{Item, ItemName};
 mod consumable;
 pub use consumable::{Consumable, ItemAction, ItemFood, instantiate_consumables};
 
+mod stellar_mission;
+pub use stellar_mission::{StellarMission, StellarMissionName};
+
 pub trait SheetData: Sized {
     const SHEET: &'static str;
     const REQUIRED_FIELDS: &[&str];
--- raphael-data/src/search.rs
+++ raphael-data/src/search.rs
@@ -1,5 +1,6 @@
 use crate::{
-    CL_ICON_CHAR, Consumable, HQ_ICON_CHAR, Locale, MEALS, POTIONS, RECIPES, get_item_name,
+    CL_ICON_CHAR, Consumable, HQ_ICON_CHAR, Locale, MEALS, POTIONS, RECIPES, STELLAR_MISSIONS,
+    get_item_name, get_stellar_mission_name,
 };
 
 fn contains_noncontiguous(string: &str, pattern: &str) -> bool {
@@ -32,6 +33,31 @@ pub fn find_recipes(search_string: &str, locale: Locale) -> Vec<u32> {
             match contains_noncontiguous(&item_name.to_lowercase(), &pattern) {
                 true => Some(*recipe_id),
                 false => None,
+            }
+        })
+        .collect()
+}
+
+pub fn find_stellar_missions(search_string: &str, locale: Locale) -> Vec<u32> {
+    let pattern = preprocess_pattern(search_string);
+    STELLAR_MISSIONS
+        .entries()
+        .filter_map(|(mission_id, mission)| {
+            let mission_name = get_stellar_mission_name(*mission_id, locale)?;
+            match contains_noncontiguous(&mission_name.to_lowercase(), &pattern) {
+                true => Some(*mission_id),
+                false => mission
+                    .recipe_ids
+                    .iter()
+                    .filter_map(|recipe_id| {
+                        let recipe = RECIPES.get(recipe_id)?;
+                        let item_name = get_item_name(recipe.item_id, false, locale)?;
+                        match contains_noncontiguous(&item_name.to_lowercase(), &pattern) {
+                            true => Some(*mission_id),
+                            false => None,
+                        }
+                    })
+                    .next(),
             }
         })
         .collect()
