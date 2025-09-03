44c24c35634da891be02cd601743b92ea8afd0d0
Trương Hoàng Long <longtruong2411@gmail.com>
2025-09-03 21:05:10 +0200

    Fix unintentional macro scroll on small displays (#211)

--- src/widgets/multiline_monospace.rs
+++ src/widgets/multiline_monospace.rs
@@ -1,9 +1,8 @@
-use std::f32;
-
 /// Copyable multiline monospace text box
 pub struct MultilineMonospace {
     text: String,
     max_height: f32,
+    scrollable: bool,
 }
 
 impl MultilineMonospace {
@@ -11,11 +10,17 @@ impl MultilineMonospace {
         Self {
             text,
             max_height: f32::INFINITY,
+            scrollable: false,
         }
     }
 
     pub fn max_height(mut self, max_height: f32) -> Self {
         self.max_height = max_height;
+        self
+    }
+
+    pub fn scrollable(mut self, scrollable: bool) -> Self {
+        self.scrollable = scrollable;
         self
     }
 }
@@ -26,7 +31,7 @@ impl egui::Widget for MultilineMonospace {
         ui.push_id(id, |ui| {
             ui.group(|ui| {
                 ui.horizontal_top(|ui| {
-                    egui::ScrollArea::vertical()
+                    egui::ScrollArea::new([self.scrollable, self.scrollable])
                         .max_height(self.max_height)
                         .show(ui, |ui| {
                             ui.monospace(&self.text);
