77455fdf9dcc365df916df8d30e4b87ec69cab0e
timothy-adam <113020023+timothy-adam@users.noreply.github.com>
2025-09-24 09:17:59 -0400

    CLI: Add ability to search for recipes based on mission (#227)
    * CLI: Add ability to search for recipes based on mission
    
    * Remove superfluous conflicts_with
    
    * Remove empty mission recipe check

--- raphael-cli/src/commands/search.rs
+++ raphael-cli/src/commands/search.rs
@@ -1,19 +1,23 @@
 use clap::{Args, ValueEnum};
-use raphael_data::{Locale, RECIPES, Recipe, get_item_name, get_job_name};
+use raphael_data::{Locale, RECIPES, Recipe, STELLAR_MISSIONS, get_item_name, get_job_name};
 
 #[derive(Args, Debug)]
 pub struct SearchArgs {
     /// Search string to use, can be partial name
-    #[arg(short, long, required_unless_present_any(["recipe_id", "item_id"]), conflicts_with_all(["recipe_id", "item_id"]))]
+    #[arg(short, long, required_unless_present_any(["recipe_id", "item_id", "mission_id"]), conflicts_with_all(["recipe_id", "item_id"]))]
     pub pattern: Option<String>,
 
     /// Recipe ID to search for
-    #[arg(short, long, required_unless_present_any(["pattern", "item_id"]), conflicts_with = "item_id")]
+    #[arg(short, long, required_unless_present_any(["pattern", "item_id", "mission_id"]), conflicts_with = "item_id")]
     pub recipe_id: Option<u32>,
 
     /// Item ID to search for
-    #[arg(short, long, required_unless_present_any(["pattern", "recipe_id"]))]
+    #[arg(short, long, required_unless_present_any(["pattern", "recipe_id", "mission_id"]))]
     pub item_id: Option<u32>,
+
+    /// Stellar mission ID to list associated recipes for
+    #[arg(long, required_unless_present_any(["pattern", "recipe_id", "item_id"]), conflicts_with_all(["pattern", "recipe_id", "item_id"]))]
+    pub mission_id: Option<u32>,
 
     /// The delimiter the output uses between fields
     #[arg(long, alias = "OFS", default_value = " ", env = "OFS")]
@@ -48,6 +52,23 @@ impl From<SearchLanguage> for Locale {
 pub fn execute(args: &SearchArgs) {
     let locale = args.language.into();
     let mut matches: Vec<(u32, &Recipe)> = Vec::new();
+
+    if let Some(mission_id_arg) = args.mission_id {
+        match STELLAR_MISSIONS.get(mission_id_arg) {
+            Some(mission) => {
+                for recipe_id in mission.recipe_ids {
+                    match RECIPES.get(*recipe_id) {
+                        Some(recipe) => matches.push((*recipe_id, recipe)),
+                        None => log::warn!("Mission {} references missing recipe id {}", mission_id_arg, recipe_id),
+                    }
+                }
+            }
+            None => {
+                println!("No such mission: {}", mission_id_arg);
+                return;
+            }
+        }
+    }
     if let Some(pattern_arg) = &args.pattern {
         matches.extend(raphael_data::find_recipes(pattern_arg, locale));
     }
