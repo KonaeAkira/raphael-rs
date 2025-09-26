4a8959e9b28a878fb72dd227e9b916d0d025ba61
augenfrosch <45207902+augenfrosch@users.noreply.github.com>
2025-09-26 21:11:12 +0200

    Reset scroll of macro text boxes on new rotation & refactor `MultilineMonospace` to use optional `id_salt` to avoid ID colissions

--- src/widgets/multiline_monospace.rs
+++ src/widgets/multiline_monospace.rs
@@ -3,6 +3,7 @@ pub struct MultilineMonospace {
     text: String,
     max_height: f32,
     scroll_direction_enabled: egui::Vec2b,
+    id_salt: Option<egui::Id>,
 }
 
 struct CopyTextButton<'a> {
@@ -37,6 +38,7 @@ impl MultilineMonospace {
             text,
             max_height: f32::INFINITY,
             scroll_direction_enabled: egui::Vec2b::default(),
+            id_salt: None,
         }
     }
 
@@ -49,28 +51,31 @@ impl MultilineMonospace {
         self.scroll_direction_enabled = direction_enabled.into();
         self
     }
+
+    pub fn id_salt(mut self, id_salt: egui::Id) -> Self {
+        self.id_salt = Some(id_salt);
+        self
+    }
 }
 
 impl egui::Widget for MultilineMonospace {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        let id = egui::Id::new(&self.text);
-        ui.push_id(id, |ui| {
-            ui.group(|ui| {
-                ui.set_max_size(ui.available_size());
-                ui.horizontal_top(|ui| {
-                    egui::ScrollArea::new(self.scroll_direction_enabled)
-                        .max_height(self.max_height)
-                        .show(ui, |ui| {
-                            ui.monospace(&self.text);
-                        });
-                    ui.put(
-                        egui::Rect::from_min_max(
-                            ui.max_rect().left_top(),
-                            ui.max_rect().right_top(),
-                        ),
-                        CopyTextButton::new(&self.text),
-                    );
+        ui.group(|ui| {
+            ui.set_max_size(ui.available_size());
+            ui.horizontal_top(|ui| {
+                let mut scroll_area = egui::ScrollArea::new(self.scroll_direction_enabled)
+                    .max_height(self.max_height);
+                if let Some(id_salt) = self.id_salt {
+                    scroll_area = scroll_area.id_salt(id_salt);
+                    ui.ctx().data_mut(|d| d.insert_temp(id_salt, ui.id()));
+                }
+                scroll_area.show(ui, |ui| {
+                    ui.monospace(&self.text);
                 });
+                ui.put(
+                    egui::Rect::from_min_max(ui.max_rect().left_top(), ui.max_rect().right_top()),
+                    CopyTextButton::new(&self.text),
+                );
             });
         })
         .response
