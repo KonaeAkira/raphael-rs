52560796033517cd1543547205a79f15db3559c4
KonaeAkira <longtruong2411@gmail.com>
2025-09-26 22:18:25 +0200

    Refactoring

--- raphael-solver/src/utils/pareto_front_builder.rs
+++ raphael-solver/src/utils/pareto_front_builder.rs
@@ -43,57 +43,12 @@ impl ParetoFrontBuilder {
     }
 
     pub fn push_slice(&mut self, values: impl Iterator<Item = ParetoValue>) {
-        let (mut slice_a, mut slice_b) = {
-            let slice_a_len = self.result.len();
-            self.result.extend(values);
-            self.result.split_at_mut(slice_a_len)
-        };
-
-        slice_b = trim_slice(slice_b, self.cutoff);
-
-        loop {
-            match (slice_a.first(), slice_b.first()) {
-                (None, None) => break,
-                (None, Some(_)) => {
-                    let idx = self.merge_buffer.last().map_or(0, |merge_tail| {
-                        slice_b.partition_point(|v| v.progress <= merge_tail.progress)
-                    });
-                    self.merge_buffer.extend_from_slice(&slice_b[idx..]);
-                    break;
-                }
-                (Some(_), None) => {
-                    let idx = self.merge_buffer.last().map_or(0, |merge_tail| {
-                        slice_a.partition_point(|v| v.progress <= merge_tail.progress)
-                    });
-                    self.merge_buffer.extend_from_slice(&slice_a[idx..]);
-                    break;
-                }
-                (Some(a), Some(b)) => {
-                    if a.quality > b.quality || (a.quality == b.quality && a.progress >= b.progress)
-                    {
-                        if self
-                            .merge_buffer
-                            .last()
-                            .is_none_or(|merge_tail| a.progress > merge_tail.progress)
-                        {
-                            self.merge_buffer.push(*a);
-                        }
-                        slice_a.split_off_first_mut();
-                    } else {
-                        if self
-                            .merge_buffer
-                            .last()
-                            .is_none_or(|merge_tail| b.progress > merge_tail.progress)
-                        {
-                            self.merge_buffer.push(*b);
-                        }
-                        slice_b.split_off_first_mut();
-                    }
-                }
-            }
-        }
-
         std::mem::swap(&mut self.result, &mut self.merge_buffer);
+        let slice_b_begin = self.merge_buffer.len();
+        self.merge_buffer.extend(values);
+        let (slice_a, mut slice_b) = self.merge_buffer.split_at_mut(slice_b_begin);
+        slice_b = trim(slice_b, self.cutoff);
+        merge(slice_a, slice_b, &mut self.result);
         self.merge_buffer.clear();
     }
 
@@ -108,7 +63,7 @@ impl ParetoFrontBuilder {
     }
 }
 
-fn trim_slice(mut slice: &mut [ParetoValue], cutoff: ParetoValue) -> &mut [ParetoValue] {
+fn trim(mut slice: &mut [ParetoValue], cutoff: ParetoValue) -> &mut [ParetoValue] {
     if slice.len() >= 1 {
         if slice[0].quality > cutoff.quality {
             while slice.len() >= 2 && slice[1].quality >= cutoff.quality {
@@ -124,4 +79,49 @@ fn trim_slice(mut slice: &mut [ParetoValue], cutoff: ParetoValue) -> &mut [Paret
         }
     }
     slice
+}
+
+fn merge(
+    mut slice_a: &mut [ParetoValue],
+    mut slice_b: &mut [ParetoValue],
+    result: &mut Vec<ParetoValue>,
+) {
+    loop {
+        match (slice_a.first(), slice_b.first()) {
+            (None, None) => break,
+            (None, Some(_)) => {
+                let idx = result.last().map_or(0, |merge_tail| {
+                    slice_b.partition_point(|v| v.progress <= merge_tail.progress)
+                });
+                result.extend_from_slice(&slice_b[idx..]);
+                break;
+            }
+            (Some(_), None) => {
+                let idx = result.last().map_or(0, |merge_tail| {
+                    slice_a.partition_point(|v| v.progress <= merge_tail.progress)
+                });
+                result.extend_from_slice(&slice_a[idx..]);
+                break;
+            }
+            (Some(a), Some(b)) => {
+                if a.quality > b.quality || (a.quality == b.quality && a.progress >= b.progress) {
+                    if result
+                        .last()
+                        .is_none_or(|merge_tail| a.progress > merge_tail.progress)
+                    {
+                        result.push(*a);
+                    }
+                    slice_a.split_off_first_mut();
+                } else {
+                    if result
+                        .last()
+                        .is_none_or(|merge_tail| b.progress > merge_tail.progress)
+                    {
+                        result.push(*b);
+                    }
+                    slice_b.split_off_first_mut();
+                }
+            }
+        }
+    }
 }
