4633ffb15841b24f3fac40c58c506eda013c5ff8
KonaeAkira <longtruong2411@gmail.com>
2025-10-20 04:12:36 +0200

    Remove dead code

--- raphael-solver/src/quality_upper_bound_solver/tests.rs
+++ raphael-solver/src/quality_upper_bound_solver/tests.rs
@@ -1,45 +1,12 @@
 use raphael_sim::*;
-use rayon::prelude::*;
 
 use crate::{
-    AtomicFlag, SolverSettings,
+    SolverSettings,
     actions::{FULL_SEARCH_ACTIONS, use_action_combo},
-    quality_upper_bound_solver::{QualityUbSolverShard, state::ReducedState},
     test_utils::*,
-    utils::ParetoValue,
 };
 
 use super::QualityUbSolver;
-
-fn solve(simulator_settings: Settings, actions: &[Action]) -> u32 {
-    let mut state = SimulationState::from_macro(&simulator_settings, actions).unwrap();
-    state.effects.set_combo(Combo::None);
-    let solver_settings = SolverSettings {
-        simulator_settings,
-        allow_non_max_quality_solutions: true,
-    };
-    let mut solver = QualityUbSolver::new(solver_settings, Default::default());
-    solver.quality_upper_bound(state).unwrap()
-}
-
-#[test]
-fn test_manipulation_refund() {
-    // https://github.com/KonaeAkira/raphael-rs/pull/128#discussion_r2062585163
-    let settings = Settings {
-        max_cp: 500,
-        max_durability: 80,
-        max_progress: 700,
-        max_quality: 20000,
-        base_progress: 100,
-        base_quality: 100,
-        job_level: 100,
-        allowed_actions: ActionMask::regular(),
-        adversarial: false,
-        backload_progress: false,
-    };
-    let result = solve(settings, &[Action::Manipulation]);
-    assert_eq!(result, 4975);
-}
 
 /// Test that the QualityUbSolver is consistent and admissible.
 /// It is consistent if the step-lb of a parent state is never greater than the step-lb of a child state.
@@ -47,14 +14,15 @@ fn test_manipulation_refund() {
 fn check_consistency(solver_settings: SolverSettings) {
     let mut solver = QualityUbSolver::new(solver_settings, Default::default());
     solver.precompute().unwrap();
+    let mut solver_shard = solver.create_shard();
     for state in generate_random_states(solver_settings, 1_000_000)
         .filter(|state| state.effects.combo() == Combo::None)
     {
-        let state_upper_bound = solver.quality_upper_bound(state).unwrap();
+        let state_upper_bound = solver_shard.quality_upper_bound(state).unwrap();
         for action in FULL_SEARCH_ACTIONS {
             let child_upper_bound = match use_action_combo(&solver_settings, state, action) {
                 Ok(child) => match child.is_final(&solver_settings.simulator_settings) {
-                    false => solver.quality_upper_bound(child).unwrap(),
+                    false => solver_shard.quality_upper_bound(child).unwrap(),
                     true if child.progress >= u32::from(solver_settings.max_progress()) => {
                         std::cmp::min(u32::from(solver_settings.max_quality()), child.quality)
                     }
