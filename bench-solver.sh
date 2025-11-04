bda9bd4bc0f9618fd87ede8d64943a4b68e85040
KonaeAkira <longtruong2411@gmail.com>
2025-11-04 21:55:01 +0100

    Add PR write permission to job

--- bench-solver.sh
+++ bench-solver.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 cmds=(
     "raphael-cli solve --recipe-id 35829 --stats 4900 4800 620 --level 100 --manipulation"
-    "raphael-cli solve --recipe-id 35830 --stats 5428 5333 737 --level 100 --manipulation --heart-and-soul"
+    # "raphael-cli solve --recipe-id 35830 --stats 5428 5333 737 --level 100 --manipulation --heart-and-soul"
 )
 
 cargo install --path raphael-cli
